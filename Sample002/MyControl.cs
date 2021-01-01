fa6ca22a8a6e45a3edfd7b90d70f0179fde61fdc
Bill Chung <bill_chungiii@hotmail.com>
2021-01-02 01:18:46 +0800

    add Sample002

---
+++ Sample002/MyControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample002
+{
+    /// <summary>
+    /// 依照步驟 1a 或 1b 執行，然後執行步驟 2，以便在 XAML 檔中使用此自訂控制項。
+    ///
+    /// 步驟 1a) 於存在目前專案的 XAML 檔中使用此自訂控制項。
+    /// 加入此 XmlNamespace 屬性至標記檔案的根項目為 
+    ///要使用的: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sample002"
+    ///
+    ///
+    /// 步驟 1b) 於存在其他專案的 XAML 檔中使用此自訂控制項。
+    /// 加入此 XmlNamespace 屬性至標記檔案的根項目為 
+    ///要使用的: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sample002;assembly=Sample002"
+    ///
+    /// 您還必須將 XAML 檔所在專案的專案參考加入
+    /// 此專案並重建，以免發生編譯錯誤: 
+    ///
+    ///     在 [方案總管] 中以滑鼠右鍵按一下目標專案，並按一下
+    ///     [加入參考]->[專案]->[瀏覽並選取此專案]
+    ///
+    ///
+    /// 步驟 2)
+    /// 開始使用 XAML 檔案中的控制項。
+    ///
+    ///     <MyNamespace:MyControl/>
+    ///
+    /// </summary>
+    public class MyControl : Control
+    {
+        static MyControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyControl), new FrameworkPropertyMetadata(typeof(MyControl)));
+        }
+    }
+}
